38696d6dbf2c342abdff66d6fc0fe12508f97579
unknown <pvig@my.centennialcollege.ca>
2018-02-06 16:28:04 -0500

    latest Commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
-var app = require('../app');
+var app = require('./app');
 var debug = require('debug')('comp308-w2018-lesson3a:server');
 var http = require('http');
 
