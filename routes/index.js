75cce1595c8361e6e8e24be79f32bd3bad269584
unknown <pvig@my.centennialcollege.ca>
2018-02-06 17:30:14 -0500

    many changes

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+/* about page */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
 module.exports = router;
